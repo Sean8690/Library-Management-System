e168015708de3b67341e596da692971cbea65543
Sean Elias <sean.s.elias@outlook.com>
2020-08-09 16:04:15 +1000

    Added using to Program.cs file

--- Library/Program.cs
+++ Library/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using LibraryData;
 
 namespace Library
